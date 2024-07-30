9848a60813f5382d92c93cc859e39767687128bc
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-07-30 15:12:51 +0200

    Raccolta dati done

--- script.js
+++ script.js
@@ -6,6 +6,30 @@ ES: marcolanciverde21*/
 //prova del collegamento del documento  alla console
 console.log('JS OK');
 
-// 1) Recupero l'elemento di interesse nel DOM
+// 1) Preparazione : Recupero l'elemento di interesse nel DOM
 const passwordElement = document.getElementById ('password');
 console.log('passwordElement');
+
+//Aggiungo variabile predeterminata NUMBER
+const number = 21;
+
+
+// 2) Raccolta dati: chiedo nome/cognome/colore preferito
+
+//NOME
+const firstName = prompt('Come ti chiami?', 'Daniele');
+console.log('firstName');
+
+//COGNOME
+const lastName = prompt('Il tuo cognome?', 'Innamorato');
+console.log('lastName');
+
+//COLORE
+const favoriteColor = prompt('Colore preferito?', 'Bordeaux');
+console.log('favouriteColor');
+
+
+
+// 2) Elaborazione dati
+
+const newPw
