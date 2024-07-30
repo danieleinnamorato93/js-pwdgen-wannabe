d7bc85e75ef34f2236fbb1f399cb5d9c8f7dd8ba
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-07-30 14:35:47 +0200

    JS on check

---
+++ script.js
@@ -0,0 +1,6 @@
+/*Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine stampa in console nomecognomecolorepreferito21
+ES: marcolanciverde21*/
+console.log('JS OK');
