b24cfec4ab6fc7728d4b4b30754955bcc6d90c9e
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-07-30 14:44:52 +0200

    elemento  d'interesse recuperato

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine stampa in console nomecognomecolorepreferito21
 ES: marcolanciverde21*/
+//prova del collegamento del documento  alla console
 console.log('JS OK');
+
+// 1) Recupero l'elemento di interesse nel DOM
+const passwordElement = document.getElementById ('password');
+console.log('passwordElement');
